a37c4e4f7d4cb7b3d753fd8b60fb6dbb7be16f9e
Joann Pan <joannlpan@gmail.com>
2020-04-29 19:20:48 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,6 +20,12 @@ WIN_COMBINATIONS =
 ]
 
 def won?(board)
-
-
+  
+  WIN_COMBINATIONS.detect do |row|
+    board[row[0]] == board[row[1]] && board[row[1]] == board[row[2]]
+    && position_taken(board, row[0])
 end
+
+
+#return false/nil if there is no winning combo present
+#return winning combination indexes as an array if there is a win
