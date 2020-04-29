a9a7dbda063ac4f61e4663f7c052c71ea2840729
Joann Pan <joannlpan@gmail.com>
2020-04-29 19:20:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,7 @@ WIN_COMBINATIONS =
 ]
 
 def won?(board)
-  
+
   WIN_COMBINATIONS.detect do |row|
     board[row[0]] == board[row[1]] && board[row[1]] == board[row[2]]
     && position_taken(board, row[0])
