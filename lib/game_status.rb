c10e3881150eaced9b56c11a6eac25589aa23cd2
Joann Pan <joannlpan@gmail.com>
2020-04-29 19:08:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,12 +4,12 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = 
+WIN_COMBINATIONS =
 
 [
 [0, 1, 2],
 [3, 4, 5],
-[6, 7, 8], 
+[6, 7, 8],
 
 [0, 3, 6],
 [1, 4, 7],
